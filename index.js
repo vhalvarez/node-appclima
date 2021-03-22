c8e74d48032e269b4a32f4acc5fe2a0d366ce762
vhalvarez <alvarez.programmer@gmail.com>
2021-03-22 15:01:18 -0400

    Modelo para controlar la aplicacion

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer.js');
+const Busquedas = require('./models/busquedas.js');
 
 const main = async () => {
+	const busquedas = new Busquedas();
 	let opt;
 
 	do {
 		opt = await inquirerMenu();
 
-		console.log({ opt });
+		switch (opt) {
+			case 1:
+				// Mostrar mensaje
+				const lugar = await leerInput('Ciudad: ');
+				console.log(lugar);
+
+				// Buscar los lugares
+
+				// Seleccionar el lugar
+
+				// Clima
+
+				// Mostrar resultados
+				console.log('Informacion del lugar\n'.green);
+				console.log('Ciudad: ');
+				console.log('Lat: ');
+				console.log('Lng: ');
+				console.log('Temperatura: ');
+				console.log('Minima: ');
+				console.log('Maxima: ');
+			break;
+		}
 
 		await pausa();
 	} while (opt !== 0);
---
+++ models/busquedas.js
@@ -0,0 +1,17 @@
+class Busquedas {
+    historial = ['Tegucigalpa', 'Madrid', 'Caracas']
+
+    constructor() {
+        // TODO: leer bd si existe
+    }
+
+    async ciudad( lugar = '') {
+        //peticion http
+
+        console.log(lugar);
+
+        return [] // retornar los lugares que coincidan
+    }
+}
+
+module.exports = Busquedas
