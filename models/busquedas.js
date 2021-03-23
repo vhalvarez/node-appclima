cc00d53cf3e0d616ee95b98550faf5e68037cb47
vhalvarez <alvarez.programmer@gmail.com>
2021-03-23 14:01:23 -0400

    Mapbox

--- models/busquedas.js
+++ models/busquedas.js
@@ -10,8 +10,11 @@ class Busquedas {
 	async ciudad(lugar = '') {
 		try {
 			//peticion http
-			const resp = axios.get();
+			const resp = await axios.get(
+				'https://api.mapbox.com/geocoding/v5/mapbox.places/cARACAS.json?access_token=pk.eyJ1IjoidmhhbHZhcmV6IiwiYSI6ImNrbW1iYTRhcDFkZjYyd3J3MTFndXVyYTUifQ.zc-k0IVILns7X7hiddl-OQ&cachebuster=1616522031166&autocomplete=true&limit=1&language=es'
+			);
 
+			console.log(resp.data);
 			return []; // retornar los lugares que coincidan
 		} catch (error) {
 			return [];
